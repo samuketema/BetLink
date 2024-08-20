c9b1756332674b84a034ab70a630b58234d42aca
Samuel <samuelketema01@gmail.com>
2024-08-20 23:04:02 +0300

    data provider.

---
+++ lib/utils/data.dart
@@ -0,0 +1,149 @@
+
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+var profile = "https://avatars.githubusercontent.com/u/86506519?v=4";
+
+List populars = [
+  {
+    "image": "https://www.betoch.et/wp-content/uploads/2024/06/B-G5-Mixed-use-commercial-building-16-835x467.jpg",
+    "name": "Single Villa",
+    "price": "28,000,000 birr",
+    "location": "Bole , Addis Ababa",
+    "is_favorited": true,
+  },
+  {
+    "image": "https://www.betoch.et/wp-content/uploads/2024/02/Residence-House-2.jpg",
+    "name": "Convertible Studio",
+    "price": "25,000,000 birr",
+    "location": "Lideta , Addis Abeba",
+    "is_favorited": false,
+  },
+  {
+    "image": "https://www.betoch.et/wp-content/uploads/2024/07/140-m2-L-shape-House-3.jpg",
+    "name": "Twin Castle",
+    "price": "17,000,000 birr",
+    "location": "Gelan , Oromiya",
+    "is_favorited": false,
+  },
+  {
+    "image": "https://allgebeya.com/wp-content/uploads/classified-listing/2023/10/2-1-657x493.jpg",
+    "name": "Twin Villa",
+    "price": "12,000,000 birr",
+    "location": "Bethel , Addis Abeba",
+    "is_favorited": false,
+  },
+];
+
+List recents = [
+  {
+    "image": "https://images.unsplash.com/photo-1549517045-bc93de075e53?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NHx8Zm9vZHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    "name": "Double Villa",
+    "price": "\$180k",
+    "location": "Phnom Penh",
+    "is_favorited": false,
+  },
+  {
+    "image": "https://images.unsplash.com/photo-1598928506311-c55ded91a20c?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NHx8Zm9vZHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    "name": "Convertible Studio",
+    "price": "\$150k",
+    "location": "Phnom Penh",
+    "is_favorited": false, 
+  },
+  {
+    "image": "https://images.unsplash.com/photo-1576941089067-2de3c901e126?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NHx8Zm9vZHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    "name": "Double Villa",
+    "price": "\$180k",
+    "location": "Phnom Penh",
+    "is_favorited": false,
+  },
+];
+
+List categories = [
+  {
+    "name" : "All",
+    "icon" :  FontAwesomeIcons.boxesStacked
+  },
+  {
+    "name" : "Villa",
+    "icon" :  FontAwesomeIcons.buildingColumns
+  },
+  {
+    "name" : "Shop",
+    "icon" :  FontAwesomeIcons.shop
+  },
+  {
+    "name" : "Building",
+    "icon" :  FontAwesomeIcons.building
+  },
+  {
+    "name" : "House",
+    "icon" :  FontAwesomeIcons.house
+  },
+];
+
+var brokers = [
+    {
+    "image": "https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjV8fHByb2ZpbGV8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60", 
+    "name": "John Siphron", 
+    "type": "Broker", 
+    "description": "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document",
+    "rate": 4, 
+  },
+  {
+    "image":"https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTF8fHByb2ZpbGV8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    "name" : "Corey Aminoff",
+    "type": "Broker", 
+    "description": "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document",
+    "rate": 4, 
+  },
+  {
+    "image" : "https://images.unsplash.com/photo-1617069470302-9b5592c80f66?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Z2lybHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    "name": "Siriya Aminoff", 
+    "type": "Broker", 
+    "description": "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document",
+    "rate": 4, 
+  },
+  {
+    "image" : "https://images.unsplash.com/photo-1545167622-3a6ac756afa4?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTB8fHByb2ZpbGV8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    "name": "Rubin Joe", 
+    "type": "Broker", 
+    "description": "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document",
+    "rate": 4, 
+  },
+];
+
+List companies = [
+  {
+    "image": "https://images.unsplash.com/photo-1549517045-bc93de075e53?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NHx8Zm9vZHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    "name": "TS Home",
+    "location": "Phnom Penh, Cambodia",
+    "type": "Broker",
+    "is_favorited": false,
+    "icon" : Icons.domain_rounded
+  },
+  {
+    "image": "https://images.unsplash.com/photo-1618221469555-7f3ad97540d6?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NHx8Zm9vZHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    "name": "Century 21",
+    "location": "Phnom Penh, Cambodia",
+    "type": "Broker",
+    "is_favorited": true,
+    "icon" : Icons.house_siding_rounded
+  },
+  {
+    "image": "https://images.unsplash.com/photo-1625602812206-5ec545ca1231?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NHx8Zm9vZHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    "name": "Dabest Pro",
+    "location": "Phnom Penh, Cambodia",
+    "type": "Broker",
+    "is_favorited": true,
+    "icon" : Icons.home_work_rounded
+  },
+  {
+    "image": "https://images.unsplash.com/photo-1625602812206-5ec545ca1231?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NHx8Zm9vZHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    "name": "Cam Reality",
+    "location": "Phnom Penh, Cambodia",
+    "type": "Broker",
+    "is_favorited": true,
+    "icon" : Icons.location_city_rounded
+  },
+];
