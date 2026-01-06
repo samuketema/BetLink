b3f37aa84fbab4b4b71f0c839522831901630e08
Samuel Ketema <samuelketema01@gmail.com>
2026-01-06 23:44:29 +0300

    main

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,6 @@ import 'package:betlink/pages/login.dart';
 import 'package:betlink/pages/register_as_agent.dart';
 import 'package:betlink/pages/registration.dart';
 import 'package:betlink/pages/welcome_screen.dart';
-import 'package:betlink/providers/user_provider.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
